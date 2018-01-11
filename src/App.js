a07e63cab90398acccea87c02de230f9ad7b7098
linh626688@gmail.com <cavoixanh>
2018-01-11 23:47:17 +0700

    done video 41

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import "./App.css";
 import Person from './Person/Person.js';
 
 class App extends Component {
+    state = {
+        person: [
+            {name: 'helix',age : 18},
+            {name: 'red',age : 38},
+            {name: 'devils',age : 28}
+        ],
+        other : 'another value'
+    }
+    switchName = () =>{
+        console.log("Was clicked")
+        // don't do this this.state.person[0].name = 'name change';
+        this.setState({
+            person: [
+                {name: 'helix edit',age : 18},
+                {name: 'red edit',age : 38},
+                {name: 'devils edit',age : 28}
+            ]
+        })
+    }
     render() {
         return (
             <div className="App">
                 <h1>Hello world</h1>
                 <p> Having something</p>
-                <Person name="LOL" age="29"/>
-                <Person name="L0000L" age="22"/>
-                <Person name="LLLLL" age="26"/>
+                <button onClick={this.switchName}>Switch</button>
+                <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+                <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
+                <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
             </div>
         );
     }
